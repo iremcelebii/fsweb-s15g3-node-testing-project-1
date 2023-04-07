fc2e512ad457a3ea57dd5772e0890f0eb3214358
İrem Çelebi <iremcelebi2404@gmail.com>
2023-04-07 19:39:28 +0300

    tamamlandııı

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/**
- * [Görev 1] nesneyiTrimle bir nesne alır ve proplarını trimler (trim; bir stringin başında ve sonunda bulunan boşlukları(whitespaces) temizlemek)
- * @param {object} obj - propları string olan bir nesne
- * @returns {object} - stringleri trimlenmiş bir nesne döndürür
- *
- * ÖRNEK
- * nesneyiTrimle({ isim: '  jane  ' }) // yeni bir nesne döndürür { isim: 'jane' }
- */
 function nesneyiTrimle(obj) {
   //!objelerin key/value larını dolaşmak için: i-->key obj[i]-->value
   for (let i in obj) {
@@ -19,15 +11,7 @@ function nesneyiTrimle(obj) {
 }
 
 // console.log(nesneyiTrimle({ isim: "  jane  " }));
-
-/**
- * [Görev 2] verileniTrimle propları string olan bir nesne alır ve gönderilen propu trimler.
- * @param {object} obj - propları string olan bir nesne
- * @returns {object} - istenilen propu trimlenmiş nesneyi döndürür
- *
- * ÖRNEK
- * verileniTrimle({ isim: '  jane  ' , yas: ' 34 '}, 'isim') // şunu döndürür { isim: 'jane', yas: ' 34 '}
- */
+//!-------------------------------------------------------------------------------------------------------
 function verileniTrimle(obj, prop) {
   obj[prop] = obj[prop].trim();
 
@@ -35,15 +19,7 @@ function verileniTrimle(obj, prop) {
 }
 
 // console.log(verileniTrimle({ isim: "  jane  ", yas: "   34    " }, "isim"));
-
-/**
- * [Görev 3] enBuyukTamsayiyiBul bir dizi nesne içinde bulunan tamsayılardan en büyük olanı bulur { tamsayi: 1 }
- * @param {object[]} tamsayilar - bir dizi nesne
- * @returns {number} - en büyük tamsayı
- *
- * ÖRNEK
- * enBuyukTamsayiyiBul([{ tamsayi: 1 }, { tamsayi: 3 }, { tamsayi: 2 }]) // 3 döndürür
- */
+//!-------------------------------------------------------------------------------------------------------
 function enBuyukTamsayiyiBul(tamsayilar) {
   let enbuyuksayi = 0;
   tamsayilar.map((obje) => {
@@ -59,29 +35,10 @@ function enBuyukTamsayiyiBul(tamsayilar) {
 console.log(
   enBuyukTamsayiyiBul([{ tamsayi: 1 }, { tamsayi: 3 }, { tamsayi: 2 }])
 );
+//!-------------------------------------------------------------------------------------------------------
 //!NESNE TABANLI PROGRAMLAMA DİLLERİ: java C# JS
 //bu bir class/obje:
 function Sayici(ilkSayi) {
-  /**
-   * [Görev 4A] Sayici bir sayaç oluşturur
-   * @param {number} ilkSayi - Sayacin ilk değeri
-   */
-
-  // ✨ gerekli propları ekleyin
-
-  /**
-   * [Görev 4B] asagiSay metodu sıfıra doğru sayar
-   * @returns {number} - bir sonraki sayı, sıfırdan küçük olamaz
-   *
-   * Örnek
-   * const sayac = new Sayici(3)
-   * sayac.asagiSay() // 3 döndürür
-   * sayac.asagiSay() // 2 döndürür
-   * sayac.asagiSay() // 1 döndürür
-   * sayac.asagiSay() // 0 döndürür
-   * sayac.asagiSay() // 0 döndürür
-   */
-
   //!objenin metodu:
   this.asagiSay = () => {
     if (ilkSayi == 0) {
@@ -98,24 +55,9 @@ console.log(sayac.asagiSay());
 console.log(sayac.asagiSay());
 console.log(sayac.asagiSay());
 console.log(sayac.asagiSay());
+
+//!-------------------------------------------------------------------------------------------------------
 function Mevsimler() {
-  /**
-   * [Görev 5A] Mevsimler , bir mevsimler nesnesi oluşturur
-   */
-
-  /**
-   * [Görev 5B] sonraki metodu bir sonraki mevsimi gösterir
-   * @returns {string} - bir sonraki mevsim "yaz" olarak yüklenir
-   *
-   * ÖRNEK
-   * const mevsimler = new Mevsimler()
-   * mevsimler.sonraki() // "yaz" döndürür
-   * mevsimler.sonraki() // "sonbahar" döndürür
-   * mevsimler.sonraki() // "kış" döndürür
-   * mevsimler.sonraki() // "ilkbahar" döndürür
-   * mevsimler.sonraki() // "yaz" döndürür
-   */
-
   const mevsimler = ["yaz", "sonbahar", "kış", "ilkbahar"];
   let i = 0;
 
@@ -133,7 +75,7 @@ function Mevsimler() {
 // console.log(mevsimler.sonraki());
 // console.log(mevsimler.sonraki());
 // console.log(mevsimler.sonraki());
-
+//!-------------------------------------------------------------------------------------------------------
 function Araba(isim, depo, kml) {
   let gidilenYol = 0;
 
@@ -179,19 +121,7 @@ focus.sur(30);
 focus.sur(180);
 focus.benzinal(100);
 focus.sur(150);
-/**
- * [Görev 7] Bir sayının çift olup olmadığını asenkron olarak çözümler
- * @param {number} sayi - kontrol edilecek sayı
- * @returns {promise} - sayı çiftse true, aksi takdirde false
- *
- * ÖRNEK
- * asenkronCiftSayi(2).then(result => {
- *    // sonuç true
- * })
- * asenkronCiftSayi(3).then(result => {
- *    // sonuç false
- * })
- */
+//!-------------------------------------------------------------------------------------------------------
 function asenkronCiftSayi(sayi) {
   return new Promise((res) => {
     res(sayi % 2 == 0);
--- index.test.js
+++ index.test.js
@@ -1,59 +1,172 @@
-const utils = require('./index')
+const utils = require("./index");
 
-describe('[Görev 1] nesneyiTrimle', () => {
-  test('[1] propları trimlenmiş bir nesne döndürüyor', () => {
+describe("[Görev 1] nesneyiTrimle", () => {
+  test("[1] propları trimlenmiş bir nesne döndürüyor", () => {
     // ÖRNEK
-    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
-    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
-    const actual = utils.nesneyiTrimle(input)
-    expect(actual).toEqual(expected)
-  })
-})
+    const input = { foo: "  foo ", bar: "bar ", baz: " baz" };
+    const expected = { foo: "foo", bar: "bar", baz: "baz" };
+    const actual = utils.nesneyiTrimle(input);
+    expect(actual).toEqual(expected);
+  });
+});
 
-describe('[Görev 2] verileniTrimle', () => {
-  // test('[3] verilen propu trimliyor', () => {})
-  // test('[4] verilen dışındaki proplar trimlenmeden döndürülüyor', () => {})
-})
+describe("[Görev 2] verileniTrimle", () => {
+  test("[3] verilen propu trimliyor", () => {
+    //!arrange
+    let inpuObj = { isim: "    jane   " };
+    let expected = { isim: "jane" };
+    //!act
+    let result = utils.verileniTrimle(inpuObj, "isim");
+    //!assert
+    expect(result).toEqual(expected);
+  });
+  test("[4] verilen dışındaki proplar trimlenmeden döndürülüyor", () => {
+    //!arrange
+    let inpuObj = { isim: "    jane   ", age: "   34   " };
+    let expected = { isim: "jane", age: "   34   " };
+    //!act
+    let result = utils.verileniTrimle(inpuObj, "isim");
+    //!assert
+    expect(result).toEqual(expected);
+  });
+});
 
-describe('[Görev 3] enBuyukTamsayiyiBul', () => {
-  // test('[5] bir dizi nesne içindeki en büyük tamsayiyi döndürüyor { tamsayi: 2 }', () => {})
-})
+describe("[Görev 3] enBuyukTamsayiyiBul", () => {
+  test("[5] bir dizi nesne içindeki en büyük tamsayiyi döndürüyor { tamsayi: 2 }", () => {
+    //!arrange
+    let inpuObj = [{ tamsayi: 1 }, { tamsayi: 3 }, { tamsayi: 2 }];
+    let expected = 3;
+    //!act
+    let result = utils.enBuyukTamsayiyiBul(inpuObj);
+    //!assert
+    expect(result).toEqual(expected);
+  });
+});
 
-describe('[Görev 4] Sayici', () => {
-  let sayici
+describe("[Görev 4] Sayici", () => {
+  let sayici;
   beforeEach(() => {
-    sayici = new utils.Sayici(3) // her test yeni bir sayı ile başlatılıyor
-  })
-  // test('[6] sayici.asagiSay ilk çağırılışında başlangıç sayışını yapıyor', () => {})
-  // test('[7] sayici.asagiSay İKİNCİ çağırılışında başlangıç eksi 1 sayıyor', () => {})
-  // test('[8] sayıcı sonunda sıfıra ulaşır ama daha aşağı saymaz', () => {})
-})
+    //!arrange
+    sayici = new utils.Sayici(3); // her test yeni bir sayı ile başlatılıyor
+  });
+  test("[6] sayici.asagiSay ilk çağırılışında başlangıç sayışını yapıyor", () => {
+    //!act
+    let result = sayici.asagiSay();
+    //!assert
+    expect(result).toEqual(3);
+  });
+  test("[7] sayici.asagiSay İKİNCİ çağırılışında başlangıç eksi 1 sayıyor", () => {
+    //!act
+    sayici.asagiSay();
+    let result = sayici.asagiSay();
+    //!assert
+    expect(result).toEqual(2);
+  });
+  test("[8] sayıcı sonunda sıfıra ulaşır ama daha aşağı saymaz", () => {
+    //!act
+    sayici.asagiSay();
+    sayici.asagiSay();
+    sayici.asagiSay();
+    sayici.asagiSay();
+    let result = sayici.asagiSay();
 
-describe('[Görev 5] Mevsimler', () => {
-  let mevsimler
+    //!assert
+    expect(result).toEqual(0);
+  });
+});
+
+describe("[Görev 5] Mevsimler", () => {
+  let mevsimler;
   beforeEach(() => {
-    mevsimler = new utils.Mevsimler() // her test yeni bir mevsimle başlar
-  })
-  // test('[9] mevsimler.sonraki İLK çağırılışında "yaz" döndürüyor', () => {})
-  // test('[10] mevsimler.sonraki İKİNCİ çağırılışında "sonbahar" döndürüyor', () => {})
-  // test('[11] mevsimler.sonraki ÜÇÜNCÜ çağırılışında "kış" döndürüyor', () => {})
-  // test('[12] mevsimler.sonraki DÖRDÜNCÜ çağırılışında "ilkbahar" döndürüyor', () => {})
-  // test('[13] mevsimler.sonraki BEŞİNCİ çağırılışında "yaz" döndürüyor', () => {})
-  // test('[14] mevsimler.sonraki KIRKINCI çağırılışında "ilkbahar" döndürüyor', () => {})
-})
+    mevsimler = new utils.Mevsimler(); // her test yeni bir mevsimle başlar
+  });
+  test('[9] mevsimler.sonraki İLK çağırılışında "yaz" döndürüyor', () => {
+    //!act
+    let result = mevsimler.sonraki();
+    //!assert
+    expect(result).toEqual("yaz");
+  });
+  test('[10] mevsimler.sonraki İKİNCİ çağırılışında "sonbahar" döndürüyor', () => {
+    //!act
+    mevsimler.sonraki();
+    let result = mevsimler.sonraki();
+    //!assert
+    expect(result).toEqual("sonbahar");
+  });
+  test('[11] mevsimler.sonraki ÜÇÜNCÜ çağırılışında "kış" döndürüyor', () => {
+    //!act
+    mevsimler.sonraki();
+    mevsimler.sonraki();
+    let result = mevsimler.sonraki();
+    //!assert
+    expect(result).toEqual("kış");
+  });
+  test('[12] mevsimler.sonraki DÖRDÜNCÜ çağırılışında "ilkbahar" döndürüyor', () => {
+    //!act
+    mevsimler.sonraki();
+    mevsimler.sonraki();
+    mevsimler.sonraki();
 
-describe('[Görev 6] Araba', () => {
-  let focus
+    let result = mevsimler.sonraki();
+    //!assert
+    expect(result).toEqual("ilkbahar");
+  });
+  test('[13] mevsimler.sonraki BEŞİNCİ çağırılışında "yaz" döndürüyor', () => {
+    //!act
+    mevsimler.sonraki();
+    mevsimler.sonraki();
+    mevsimler.sonraki();
+    mevsimler.sonraki();
+    let result = mevsimler.sonraki();
+    //!assert
+    expect(result).toEqual("yaz");
+  });
+  test('[14] mevsimler.sonraki KIRKINCI çağırılışında "ilkbahar" döndürüyor', () => {
+    //!act
+    for (let i = 1; i <= 39; i++) {
+      mevsimler.sonraki();
+    }
+    let result = mevsimler.sonraki();
+    //!assert
+    expect(result).toEqual("ilkbahar");
+  });
+});
+
+describe("[Görev 6] Araba", () => {
+  let focus;
   beforeEach(() => {
-    focus = new utils.Araba('focus', 20, 30) // her test yeni bir araba oluşturur
-  })
-  // test('[15] arabayı sürünce güncellenmiş odometer döndürüyor', () => {})
-  // test('[16] arabayı sürmek benzin tüketiyor', () => {})
-  // test('[17] benzinalma arabayı sürmeye izin veriyor', () => {})
-  // test('[18] dolu depoya benzin alma etki etmiyor', () => {})
-})
+    focus = new utils.Araba("focus", 20, 30); // her test yeni bir araba oluşturur
+  });
+  test("[15] arabayı sürünce güncellenmiş odometer döndürüyor", () => {
+    let result = focus.sur(30);
+    expect(result).toEqual(30);
+  });
+  test("[16] arabayı sürmek benzin tüketiyor", () => {
+    focus.sur(600);
+    let result = focus.sur(600);
+    expect(result).toEqual(600);
+  });
+  test("[17] benzinalma arabayı sürmeye izin veriyor", () => {
+    focus.sur(600);
+    focus.benzinal(20);
+    let result = focus.sur(600);
+    expect(result).toEqual(1200);
+  });
+  test("[18] dolu depoya benzin alma etki etmiyor", () => {
+    let result1 = focus.benzinal(20);
+    let result2 = focus.benzinal(40);
+    expect(result1).toEqual(result2);
+  });
+});
 
-describe('[Görev 7] asenkronCiftSayi', () => {
-  // test('[19] bir çift sayı verilirse true çözümlüyor', () => {})
-  // test('[20] tek sayı verilirse false çözümlüyor', () => {})
-})
+describe("[Görev 7] asenkronCiftSayi", () => {
+  test("[19] bir çift sayı verilirse true çözümlüyor", () => {
+    utils.asenkronCiftSayi(2).then((result) => {
+      expect(result).toBe(true);
+    });
+  });
+  test("[20] tek sayı verilirse false çözümlüyor", async () => {
+    let result = await utils.asenkronCiftSayi(1);
+    expect(result).toBe(false);
+  });
+});
